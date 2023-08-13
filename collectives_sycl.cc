6d8545ac35f074ae26888d01367742eb511093fa
Maozhou Ge <gmc2.each@gmail.com>
2023-08-13 12:38:18 +0800

    add RingAllgather

--- collectives_sycl.cc
+++ collectives_sycl.cc
@@ -1,6 +1,7 @@
 #include "collectives.h"
 
 #include <cstring>
+#include <numeric>
 #include <stdexcept>
 #include <vector>
 
@@ -183,4 +184,50 @@ void RingAllreduce(float* data, size_t length, float* output) {
   }
 
   dealloc(recv_buf);
+}
+
+void RingAllgather(float* data, size_t length, float* output) {
+  int rank, size;
+  if (MPI_Comm_rank(MPI_COMM_WORLD, &rank) != MPI_SUCCESS) {
+    throw std::runtime_error("MPI_Comm_rank failed with an error");
+  }
+  if (MPI_Comm_size(MPI_COMM_WORLD, &size) != MPI_SUCCESS) {
+    throw std::runtime_error("MPI_Comm_size failed with an error");
+  }
+
+  std::vector<size_t> segment_sizes = AllgatherInputLength(size, length);
+  size_t total_len = std::accumulate(segment_sizes.begin(), segment_sizes.end(), 0);
+
+  std::vector<size_t> segment_ends(size);
+  segment_ends[0] = segment_sizes[0];
+  for (size_t i = 1; i < segment_ends.size(); ++i) {
+    segment_ends[i] = segment_sizes[i] - segment_ends[i - 1];
+  }
+
+  float* output = alloc(total_len);
+  copy(output + segment_ends[rank] - segment_sizes[rank],
+       data, segment_sizes[rank]);
+
+  const size_t recv_from = (rank - 1 + size) % size;
+  const size_t send_to = (rank + 1) % size;
+
+  MPI_Datatype datatype = MPI_FLOAT;
+  MPI_Status recv_status;
+
+  // At the i'th iteration, sends segment (rank + 1 - i) and receives segment (rank - i).
+  for (int i = 0; i < size - 1; ++i) {
+    int send_chunk = (rank - i + size) % size;
+    int recv_chunk = (rank - i - 1 + size) % size;
+
+    // at every iteration we send segment (r+1-i)
+    size_t send_start = segment_ends[send_chunk] - segment_sizes[send_chunk];
+    float* segment_send = &output[send_start];
+    // at every iteration we receive segment (r-i)
+    size_t recv_start = segment_ends[recv_chunk] - segment_sizes[recv_chunk];
+    float* segment_recv = &output[recv_start];
+
+    MPI_Sendrecv(segment_send, segment_sizes[send_chunk], datatype, send_to, 0,
+                 segment_recv, segment_sizes[recv_chunk], datatype, recv_from, 0,
+                 MPI_COMM_WORLD, &recv_status);
+  }
 }
